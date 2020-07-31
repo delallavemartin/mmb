fbd0846ace4e5cec657e4820b01302509b8cf4b5
delallavemartin <delallavemartin@gmail.com>
2020-07-31 18:52:44 -0300

    comments added

--- mb/mb.go
+++ mb/mb.go
@@ -17,6 +17,8 @@ func sendToConsumers(msg string) {
 }
 
 func dequeAndSendToConsumers(c chan string) {
+	// - Select: blocks until one of its cases can run, then it executes that case. 
+	// 	It chooses one at random if multiple are ready.
 	select {
 	case msg := <-c:
 		fmt.Println("Message: ", msg, " Deque")
@@ -30,7 +32,13 @@ func main() {
 
 	fmt.Println("Server started.")
 
+
+	// - Regular Channels : Sends and receives block until the other side is ready. 
+	// 	This allows goroutines to synchronize without explicit locks or condition variables.
+	// - Buffered Channel : Sends to a buffered channel block only when the buffer is full. Receives block when the buffer is empty.
 	c := make(chan string)
+	//  Only the sender should close a channel, never the receiver. Sending on a closed channel will cause a panic.
+	//	Closing is only necessary when the receiver must be told there are no more values coming, such as to terminate a range loop.
 	defer close(c)
 
 	buffer := make([]byte, 1024)
@@ -38,6 +46,8 @@ func main() {
 	for {
 		n, addr, _ := serverConn.ReadFromUDP(buffer)
 		fmt.Println("Received ", string(buffer[0:n]), " from ", addr)
+		// - Goroutine:  is a lightweight thread managed by the Go runtime.
+		// 	Run in the same address space, so access to shared memory must be synchronized. 
 		go publish(string(buffer[0:n]), c)
 		dequeAndSendToConsumers(c)
 	}

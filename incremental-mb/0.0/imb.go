3374417dad3223f338ae8521a4bf8991c76545d5
delallavemartin <delallavemartin@gmail.com>
2020-08-10 19:35:41 -0300

    small refactor

--- incremental-mb/0.0/imb.go
+++ incremental-mb/0.0/imb.go
@@ -6,27 +6,29 @@ import (
 	"net/http"
 )
 
-func postMsg(reader io.Reader) int {
-	resp, err := http.Post("http://localhost:9999/notify", "text/plain", reader)
+func postMsg(url string, contentType string, reader io.Reader) int {
+	resp, err := http.Post(url, contentType, reader)
 	if err != nil {
-		fmt.Println("ERROR - send request to port: 9999 FAILED")
+		fmt.Println("ERROR - send request to port: ", url, " FAILED")
 		return 500
 	}
-	fmt.Println("INFO - message SUCCESFULLY sent to port: 9999")
+	fmt.Println("INFO - message SUCCESFULLY sent to: ", url)
 	defer resp.Body.Close()
 	return 200
 }
 
-func sendToHandler(w http.ResponseWriter, r *http.Request) {
-	statusCode := postMsg(r.Body)
+func publisherHandler(w http.ResponseWriter, r *http.Request) {
+	statusCode := postMsg("http://localhost:9999/notify", "text/plain", r.Body)
 	w.WriteHeader(statusCode)
 }
 
 func main() {
 	fmt.Println("INFO - Server started.")
 
-	http.HandleFunc("/notify", sendToHandler)
+	// When server receives a notification, the msg will be published to his subscribers/consumers
+	http.HandleFunc("/notify", publisherHandler)
 
+	// Each request its mapped to one lightweight thread trough go routines.
 	http.ListenAndServe(":8080", nil)
 
 }

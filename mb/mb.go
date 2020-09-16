f1d9aec7579949aa95115ade33475c9ec115405d
delallavemartin <delallavemartin@gmail.com>
2020-09-16 19:02:29 -0300

    OO approach added to 0.3 imb version

--- mb/mb.go
+++ mb/mb.go
@@ -1,56 +1,158 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
 )
 
-func publish(msg string, c chan string) {
-	c <- msg
-	fmt.Println("Msg: ",msg," Published to ", c, "channel")
+// MODEL DEFINITION
+
+// Store all information needed to publish the request.
+type Request struct {
+	Url         string
+	ContentType string
+	Reader      io.Reader
 }
 
-func sendToConsumers(msg string) {
-	conn, _ := net.DialUDP("udp", nil, &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 9999, Zone: ""})
-	defer conn.Close()
-	fmt.Fprintf(conn,msg)
+// SubscribersList with Sync.
+type SubscribersList struct {
+	Addresses []string
+	Mux       sync.Mutex
 }
 
-func dequeAndSendToConsumers(c chan string) {
-	// - Select: blocks until one of its cases can run, then it executes that case. 
-	// 	It chooses one at random if multiple are ready.
-	// select {
-	// case msg := <-c:
-	msg := <-c
-	fmt.Println("Message: ", msg, " Deque")
-	go sendToConsumers(msg)
-	// }
+// SubscribersList methods
+func (self *SubscribersList) Add(address string) {
+	self.Mux.Lock()
+	self.Addresses = append(self.Addresses, address)
+	self.Mux.Unlock()
+}
+
+func (self *SubscribersList) NotifySubscribers(notifier func(address string)) {
+	self.Mux.Lock()
+	defer self.Mux.Unlock()
+	//Iterate over subscribers list.
+	for i := 0; i < len(self.Addresses); i++ {
+		notifier(self.Addresses[i])
+	}
+}
+
+// Delivery, delivers notifications to subscribers
+type Delivery interface {
+	Delivers()
+}
+
+// HttpDelivery, implementation of HTTP communication
+type HttpDelivery struct {
+	Request Request
+}
+
+// HttpDelivery methods
+func (self *HttpDelivery) Delivers() {
+	_, err := http.Post(self.Request.Url, self.Request.ContentType, self.Request.Reader)
+	if err != nil {
+		log.Println("ERROR - send request to: ", self.Request.Url, " FAILED")
+		log.Println(err)
+		return
+	}
+	log.Println("INFO - message SUCCESFULLY sent to: ", self.Request.Url)
+}
+
+// Notification Center, knows how to notify and received messages
+type NotificationCenter struct {
+	Channel chan Request
+}
+
+// Notification Center methods
+func (self *NotificationCenter) OnMessageReceived(router func(request Request)) {
+	// this loop receives values from the channel repeatedly until it is closed
+	for request := range self.Channel {
+		router(request)
+	}
+}
+
+func (self *NotificationCenter) Notifier(msg string) func(address string) {
+	return func(address string) {
+		// Send request to the channel in order to proccess it.
+		self.Channel <- Request{"http://localhost:" + address + "/notify", "text/plain", strings.NewReader(msg)}
+	}
+}
+
+// CustomReader, used to convert a Reader to string
+type CustomReader struct {
+	Reader         io.Reader
+	ReaderAsString string
+}
+
+// CustomReader Methods
+func (self *CustomReader) ToString() string {
+	if self.ReaderAsString == "" {
+		stream, err := ioutil.ReadAll(self.Reader)
+		if err != nil {
+			log.Println("ERROR - reading reader FAILED")
+			return ""
+		}
+		self.ReaderAsString = string(stream)
+	}
+	return self.ReaderAsString
+}
+
+// Global Variables
+// SPEC: Only 10 subscribers are supported
+var aListOfSubscribers = SubscribersList{Addresses: make([]string, 0, 10)}
+
+// Notification Center, used to notify to each subscriber
+var SubscribersNotificationCenter = NotificationCenter{Channel: make(chan Request)}
+
+// Request Handlers
+func publisherHandler(w http.ResponseWriter, r *http.Request) {
+	// Reader converted to string to create one Reader per POST.
+	msg := CustomReader{Reader: r.Body}
+
+	go SubscribersNotificationCenter.OnMessageReceived(func(request Request) {
+		// OBJECT INITIALIZATION & MESSAGE
+		anHTTPDelivery := HttpDelivery{Request: request}
+		// go routines added to improve request per second performance.
+		go anHTTPDelivery.Delivers()
+	})
+
+	//CLOSURE
+	aListOfSubscribers.NotifySubscribers(SubscribersNotificationCenter.Notifier(msg.ToString()))
+}
+
+func subscriberHandler(w http.ResponseWriter, r *http.Request) {
+	// Read port number
+	portNumber := CustomReader{Reader: r.Body}
+
+	// Append new subscribers to subscribers list
+	aListOfSubscribers.Add(portNumber.ToString())
+	log.Println("INFO - port added: " + portNumber.ToString())
 }
 
 func main() {
-	serverConn, _ := net.ListenUDP("udp", &net.UDPAddr{IP: []byte{0, 0, 0, 0}, Port: 48772, Zone: ""})
-	defer serverConn.Close()
+	log.Println("INFO - Server started.")
 
-	fmt.Println("Server started.")
-
-
-	// - Regular Channels : Sends and receives block until the other side is ready. 
-	// 	This allows goroutines to synchronize without explicit locks or condition variables.
-	// - Buffered Channel : Sends to a buffered channel block only when the buffer is full. Receives block when the buffer is empty.
-	c := make(chan string)
-	//  Only the sender should close a channel, never the receiver. Sending on a closed channel will cause a panic.
-	//	Closing is only necessary when the receiver must be told there are no more values coming, such as to terminate a range loop.
-	defer close(c)
-
-	buffer := make([]byte, 1024)
-	
-	for {
-		n, addr, _ := serverConn.ReadFromUDP(buffer)
-		fmt.Println("Received ", string(buffer[0:n]), " from ", addr)
-		// - Goroutine:  is a lightweight thread managed by the Go runtime.
-		// 	Run in the same address space, so access to shared memory must be synchronized. 
-		go publish(string(buffer[0:n]), c)
-		dequeAndSendToConsumers(c)
+	// Set log file
+	// TODO refactor logging.
+	f, err := os.OpenFile("log/imb.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
 	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	// When server receives a notification, the msg will be published to his subscribers
+	// TODO: handle HTTP error codes.
+	http.HandleFunc("/notify", publisherHandler)
+
+	// When server receives a subscription, port will be added to subscribers list
+	http.HandleFunc("/subscribe", subscriberHandler)
+
+	// Each request its mapped to one lightweight thread trough go routines.
+	http.ListenAndServe(":8080", nil)
 
 }
